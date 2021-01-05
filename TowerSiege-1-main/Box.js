586f277255da03d6c77fea64df52e577c04b7b6c
Rishabh ghalyan <72549082+RishabhGhalayan@users.noreply.github.com>
2021-01-05 18:14:20 +0530

    Add files via upload

---
+++ TowerSiege-1-main/Box.js
@@ -0,0 +1,35 @@
+class Box {
+    constructor(x,y,width,height) {
+      var options = {
+          isStatic: false
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+      this.image = loadImage("wood1.png");
+      this.visiblity = 255
+
+    }
+    display(){
+      var pos =this.body.position;
+     
+      imageMode(CENTER);
+      fill("brown");
+      image(this.image,pos.x, pos.y, this.width, this.height);
+
+      if(this.body.speed>15){
+
+     this.body.position.x = 15055
+      push()
+      this.visiblity -=10
+       tint(0,this.visiblity)
+       image(this.image,this.body.position.x,this.body.position.y,50,50)
+       pop()
+
+
+      }
+
+  
+    }
+  };
